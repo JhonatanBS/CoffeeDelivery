41fb8321196c7a696c298667d4f971dd5656ce7d
JhonatanBS <jhonatanb.s4nt0s@gmail.com>
2023-09-04 13:03:44 -0300

    The Project was created with Native Base library and added the Provider with colors

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+import { Center, NativeBaseProvider, Text } from 'native-base';
+
+import { THEME } from './src/theme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <Center bg={THEME.colors.purple_dark} flex={1}>
+        <Text textAlign="center" color="white">
+          Hello World
+        </Text>
+      </Center>
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/theme/index.ts
@@ -0,0 +1,30 @@
+import { extendTheme } from "native-base";
+
+export const THEME = extendTheme({
+  colors: {
+    yellow_dark: "#C47F17",
+    yellow: "#DBAC2C",
+
+    purple_dark: "#4B2995",
+    purple: "#8047F8",
+    purple_light: "#EBE5F9",
+
+    gray: {
+      100: "#272221",
+      200: "#403937",
+      300: "#574F4D",
+      400: "#8D8686",
+      500: "#D7D5D5",
+      600: "#E6E5E5",
+      700: "#EDEDED",
+      800: "#F3F2F2",
+      900: "#FAFAFA",
+    },
+
+    white: "#FFFFFF",
+
+    red_dark: "#C44117",
+    red: "#E8BAAB",
+    red_light: "#F2DFD8"
+  }
+});
