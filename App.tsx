082a530fa3a440344b007f34b0ae7837c3942c29
JhonatanBS <jhonatanb.s4nt0s@gmail.com>
2023-09-04 22:35:37 -0300

    Created screen Splash when the application open

--- App.tsx
+++ App.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { NativeBaseProvider  } from 'native-base';
+import { NativeBaseProvider, StatusBar } from 'native-base';
 
 import { Roboto_400Regular, Roboto_700Bold, useFonts} from "@expo-google-fonts/roboto";
 import { Baloo2_700Bold} from "@expo-google-fonts/baloo-2";
 
 import { THEME } from './src/theme';
+import { Splash } from './src/screens/Splash';
+import { Home } from './src/screens/Home';
+
+import { preventAutoHideAsync } from "expo-splash-screen";
+
+preventAutoHideAsync();
 
 export default function App() {
-  const [ fontsLoaded ] = useFonts({ Roboto_400Regular, Roboto_700Bold,Baloo2_700Bold})
+  const [ fontsLoaded ] = useFonts({ Roboto_400Regular, Roboto_700Bold,Baloo2_700Bold});
+  const [splashComplete, setSplashComplete] = useState(false);
+
   return (
     <NativeBaseProvider theme={THEME}>
-      
+
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {splashComplete && fontsLoaded ? < Home /> : <Splash onComplete={setSplashComplete}/>}
     </NativeBaseProvider>
   );
 }
---
+++ src/screens/Home.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from "native-base";
+
+export function Home() {
+  return(
+   <Center flex={1} bg="#FFF">
+    <Text size={10}>
+      Home
+    </Text>
+   </Center>
+  )
+}
